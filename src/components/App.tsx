08aa0c5fb8a343ec63b720b588b946ad47301af4
boonecrockett <97560768+boonecrockett@users.noreply.github.com>
2025-03-10 15:47:05 -0600

    Update App component with routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Layout from './Layout'
+import Home from '../pages/Home'
+import LoginPage from '../pages/LoginPage'
+import AuthCallback from '../pages/AuthCallback'
+import Admin from '../pages/Admin'
+import BracketPage from '../pages/BracketPage'
 
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <div className="container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold text-center mb-8">BracketVote</h1>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="callback" element={<AuthCallback />} />
+          <Route path="admin" element={<Admin />} />
+          <Route path="bracket" element={<BracketPage />} />
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
